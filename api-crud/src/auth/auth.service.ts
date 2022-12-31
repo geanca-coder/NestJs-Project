3c95529cc050e43893190a457968f0b223f108b7
geanca-coder <geancarlosberm54@gmail.com>
2022-12-31 17:27:55 -0600

    adding dtos and setting database with prisma

---
+++ api-crud/src/auth/auth.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+
+
+@Injectable()
+export class AuthService{
+    constructor(private prisma: PrismaService){
+
+    }
+    signup(){
+        return {
+            msg : 'I am signed up'
+        }
+    }
+
+
+    signin(){
+        return {
+            msg : 'You are logged in'
+        }
+    }
+
+}
+
