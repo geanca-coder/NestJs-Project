e7106322b6c367465ffd3025f99a6dc84cebc3ba
geanca-coder <geancarlosberm54@gmail.com>
2023-01-01 22:44:46 -0600

    jwt functionaliy

--- api-crud/src/auth/auth.controller.ts
+++ api-crud/src/auth/auth.controller.ts
@@ -12,12 +12,11 @@ export class AuthController{
     @Post('signup')
     signup(@Body() dto : AuthDto ){
        //DTOS
-       console.log(dto)
        return this.authService.signup(dto)
     }
 
     @Post('signin')
-    signin(){
-        return this.authService.signin()
+    signin(@Body() dto : AuthDto ){
+        return this.authService.signin(dto)
     }
 }
